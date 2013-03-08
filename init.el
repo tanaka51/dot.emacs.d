1ac27a0de96c56bf2a0f26f32adc01ee7ee4cd03
Koichi Tanaka <k.tanaka@everyleaf.com>
2013-03-08 21:04:19 +0900

    Align

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 
 ;; package
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa"     . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
