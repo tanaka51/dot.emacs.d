9ad145b2211c547c345d29956811ae397c167e85
Koichi Tanaka <k.tanaka@everyleaf.com>
2013-06-27 14:40:06 +0900

    Add el-get setting

--- init.el
+++ init.el
@@ -60,3 +60,17 @@
 
  ;; Windows
  ((string-match "mingw" system-configuration)))
+
+
+;; el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/config/el-get/recipes")
+(el-get 'sync)
